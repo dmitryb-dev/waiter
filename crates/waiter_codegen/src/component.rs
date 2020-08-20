d4951c356f6e95b0f6ca2423b7e4fcf58b831578
dmitrybarannik <dmitryb.dev@gmail.com>
2020-08-20 20:27:51 +0300

    Full support of Rc and Box

--- crates/waiter_codegen/src/component.rs
+++ crates/waiter_codegen/src/component.rs
@@ -1,6 +1,6 @@
 use proc_macro::{TokenStream};
 use syn::{Ident, ItemStruct, Fields, Field, Type, Error};
-use syn::export::{TokenStream2, Span};
+use syn::export::{TokenStream2, Span, ToTokens};
 use syn::spanned::Spanned;
 
 pub fn generate_component_impl(component: ItemStruct) -> TokenStream {
@@ -69,17 +69,44 @@ fn generate_dependencies_create_code(fields: Vec<&Field>) -> TokenStream2 {
 fn generate_dependency_create_code(typ: &Type, pos: usize) -> TokenStream2 {
     let dep_var_name = quote::format_ident!("dep_{}", pos);
 
-    if let Type::Reference(type_ref) = typ {
-        let referenced_type = &type_ref.elem;
-        quote::quote! {
-            let #dep_var_name = unsafe {
-                (Provider::<#referenced_type>::get_ref(container) as *const #referenced_type)
-                    .as_ref()
-                    .unwrap()
+    match typ {
+        Type::Reference(type_ref) => {
+            let referenced_type = &type_ref.elem;
+            return quote::quote! {
+                let #dep_var_name = unsafe {
+                    (Provider::<#referenced_type>::get_ref(container) as *const #referenced_type)
+                        .as_ref()
+                        .unwrap()
+                };
             };
         }
-    } else {
-        Error::new(
+        Type::Path(path_type) => {
+            let ptr_type = path_type.path.to_token_stream().to_string();
+
+            if ptr_type.starts_with("std :: rc :: Rc <") {
+                let referenced_type = &path_type.path.segments[2].arguments;
+                return quote::quote! {
+                    let #dep_var_name = Provider::#referenced_type::get(container);
+                }
+            } else if ptr_type.starts_with("Rc <") {
+                let referenced_type = &path_type.path.segments[0].arguments;
+                return quote::quote! {
+                    let #dep_var_name = Provider::#referenced_type::get(container);
+                }
+            } else if ptr_type.starts_with("Box <") {
+                let referenced_type = &path_type.path.segments[0].arguments;
+                return quote::quote! {
+                    let #dep_var_name = Provider::#referenced_type::create(container);
+                }
+            } else {
+                eprintln!("{}", typ.to_token_stream());
+                return Error::new(
+                    typ.span(),
+                    "Only &, Rc, Component and #[prop(\"name\"] number/string can be injected"
+                ).to_compile_error()
+            }
+        }
+        _ => Error::new(
             typ.span(),
             "Only &, Rc, Component and #[prop(\"name\"] number/string can be injected"
         ).to_compile_error()
